b980f03fdc28ad5e9e385d573e6d06e4d52729cf
Daniel Thornburgh <dthorn@google.com>
2025-07-15 15:24:53 -0700

    Add missing file header

--- libc/src/stdio/printf_core/float_impl.cpp
+++ libc/src/stdio/printf_core/float_impl.cpp
@@ -1,3 +1,18 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file instantiates the functionality needed for supporting floating
+/// point arguments in modular printf builds. Non-modular printf builds
+/// implicitly instantiate these functions.
+///
+//===----------------------------------------------------------------------===//
+
 #ifdef LIBC_COPT_PRINTF_MODULAR
 #include "src/__support/arg_list.h"
 
