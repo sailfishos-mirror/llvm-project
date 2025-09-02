2c96e181bb5b5a0fe04af5a43ca02d6dd634eae1
Sergei Barannikov <barannikov88@gmail.com>
2025-09-02 19:38:17 +0300

    Require that the generated functions return DecodeStatus

--- llvm/utils/TableGen/DecoderEmitter.cpp
+++ llvm/utils/TableGen/DecoderEmitter.cpp
@@ -1109,7 +1109,8 @@ void DecoderTableBuilder::emitBinaryParser(raw_ostream &OS, indent Indent,
     if (IgnoreNonDecodableOperands)
       return;
     assert(!OpInfo.Decoder.empty());
-    OS << Indent << OpInfo.Decoder << "(MI, Decoder);\n";
+    OS << Indent << "if (!Check(S, " << OpInfo.Decoder << "(MI, Decoder)))\n"
+       << Indent << "  return MCDisassembler::Fail;\n";
     return;
   }
 
