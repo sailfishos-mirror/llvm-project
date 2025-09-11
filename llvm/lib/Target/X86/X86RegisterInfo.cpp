82cedd665537555be843ca2bb92026c701298416
Matt Arsenault <Matthew.Arsenault@amd.com>
2025-09-11 21:24:16 +0900

    new triple checks

--- llvm/lib/Target/X86/X86RegisterInfo.cpp
+++ llvm/lib/Target/X86/X86RegisterInfo.cpp
@@ -53,15 +53,14 @@ static cl::opt<bool>
 extern cl::opt<bool> X86EnableAPXForRelocation;
 
 X86RegisterInfo::X86RegisterInfo(const Triple &TT)
-    : X86GenRegisterInfo(
-          (TT.getArch() == Triple::x86_64 ? X86::RIP : X86::EIP),
-          X86_MC::getDwarfRegFlavour(TT, false),
-          X86_MC::getDwarfRegFlavour(TT, true),
-          (TT.getArch() == Triple::x86_64 ? X86::RIP : X86::EIP)) {
+    : X86GenRegisterInfo((TT.isX86_64() ? X86::RIP : X86::EIP),
+                         X86_MC::getDwarfRegFlavour(TT, false),
+                         X86_MC::getDwarfRegFlavour(TT, true),
+                         (TT.isX86_64() ? X86::RIP : X86::EIP)) {
   X86_MC::initLLVMToSEHAndCVRegMapping(this);
 
   // Cache some information.
-  Is64Bit = TT.getArch() == Triple::x86_64;
+  Is64Bit = TT.isX86_64();
   IsWin64 = Is64Bit && TT.isOSWindows();
   IsUEFI64 = Is64Bit && TT.isUEFI();
 
