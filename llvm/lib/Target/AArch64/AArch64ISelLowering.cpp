1cf713917d76737a692cf5552ec4dc2e2c52b502
Anatoly Trosinenko <atrosinenko@accesssoftek.com>
2025-09-15 20:13:13 +0300

    [AArch64][PAC] Reset `killed` operand flag in fixupPtrauthDiscriminator
    Conservatively reset `killed` flag on the AddrDisc operand when it is
    updated by fixupPtrauthDiscriminator function.

--- llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
+++ llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
@@ -3485,6 +3485,9 @@ void AArch64TargetLowering::fixupPtrauthDiscriminator(
     AddrDisc = TmpReg;
   }
 
+  if (AddrDiscOp.getReg() != AddrDisc)
+    AddrDiscOp.setIsKill(false);
+
   AddrDiscOp.setReg(AddrDisc);
   IntDiscOp.setImm(IntDisc);
 }
