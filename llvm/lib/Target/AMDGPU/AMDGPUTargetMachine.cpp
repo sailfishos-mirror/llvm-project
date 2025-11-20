5eefe1f29a8d5bd6bbf63570d55b4cd879bbf94b
vikhegde <vikram.hegde@amd.com>
2025-11-20 20:08:14 +0530

    review comment

--- llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
@@ -2396,9 +2396,8 @@ void AMDGPUCodeGenPassBuilder::addPreEmitPass(AddMachinePass &addPass) const {
 
   addPass(SIModeRegisterPass());
 
-  if (TM.getOptLevel() > CodeGenOptLevel::None) {
+  if (TM.getOptLevel() > CodeGenOptLevel::None)
     addPass(SIInsertHardClausesPass());
-  }
 
   addPass(SILateBranchLoweringPass());
 
