c8f6bc4398347209bf2af7da166d96cddc3f9a90
Matt Arsenault <Matthew.Arsenault@amd.com>
2026-01-15 20:22:44 +0100

    one use check

--- llvm/lib/Transforms/InstCombine/InstCombineSimplifyDemanded.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineSimplifyDemanded.cpp
@@ -2943,7 +2943,7 @@ Value *InstCombinerImpl::SimplifyDemandedUseFPClass(Instruction *I,
   }
   case Instruction::ExtractValue: {
     Value *ExtractSrc;
-    if (match(I, m_ExtractValue<0>(m_Value(ExtractSrc)))) {
+    if (match(I, m_ExtractValue<0>(m_OneUse(m_Value(ExtractSrc))))) {
       if (auto *II = dyn_cast<IntrinsicInst>(ExtractSrc)) {
         const Intrinsic::ID IID = II->getIntrinsicID();
         switch (IID) {
