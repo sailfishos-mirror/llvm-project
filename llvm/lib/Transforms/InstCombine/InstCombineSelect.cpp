96cffbf661e0ffe47a509305fbf56f5ce5d3f54b
Snehasish Kumar <snehasishk@google.com>
2026-02-03 00:46:10 +0000

    [InstCombine] Propagate profile metadata when factoring logic operations
    When factoring logic operations involving logical AND/OR (represented as selects), InstCombine creates new select instructions. This patch ensures that these new instructions receive 'unknown' profile metadata if the function has profile data, satisfying profile verification requirements.
    
    This fixes profile verification failures in .

--- llvm/lib/Transforms/InstCombine/InstCombineSelect.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineSelect.cpp
@@ -3570,10 +3570,17 @@ Instruction *InstCombinerImpl::foldSelectOfBools(SelectInst &SI) {
                                   Value *InnerVal,
                                   bool SelFirst = false) -> Instruction * {
         Value *InnerSel = Builder.CreateSelect(InnerCond, One, InnerVal);
+        if (!ProfcheckDisableMetadataFixes)
+          if (auto *I = dyn_cast<Instruction>(InnerSel))
+            setExplicitlyUnknownBranchWeightsIfProfiled(*I, DEBUG_TYPE, &F);
+
         if (SelFirst)
           std::swap(Common, InnerSel);
         if (FalseLogicAnd || (CondLogicAnd && Common == A))
-          return SelectInst::Create(Common, InnerSel, Zero);
+          return ProfcheckDisableMetadataFixes
+                     ? SelectInst::Create(Common, InnerSel, Zero)
+                     : createSelectInstWithUnknownProfile(Common, InnerSel,
+                                                          Zero);
         else
           return BinaryOperator::CreateAnd(Common, InnerSel);
       };
@@ -3618,10 +3625,17 @@ Instruction *InstCombinerImpl::foldSelectOfBools(SelectInst &SI) {
                                  Value *InnerVal,
                                  bool SelFirst = false) -> Instruction * {
         Value *InnerSel = Builder.CreateSelect(InnerCond, InnerVal, Zero);
+        if (!ProfcheckDisableMetadataFixes)
+          if (auto *I = dyn_cast<Instruction>(InnerSel))
+            setExplicitlyUnknownBranchWeightsIfProfiled(*I, DEBUG_TYPE, &F);
+
         if (SelFirst)
           std::swap(Common, InnerSel);
         if (TrueLogicOr || (CondLogicOr && Common == A))
-          return SelectInst::Create(Common, One, InnerSel);
+          return ProfcheckDisableMetadataFixes
+                     ? SelectInst::Create(Common, One, InnerSel)
+                     : createSelectInstWithUnknownProfile(Common, One,
+                                                          InnerSel);
         else
           return BinaryOperator::CreateOr(Common, InnerSel);
       };
