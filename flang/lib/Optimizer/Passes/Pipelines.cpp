7bfd1d9a4bdb36667fc0c39d161a4f0409adcd77
Sergio Afonso <Sergio.AfonsoFumero@amd.com>
2026-02-05 11:52:25 +0000

    move stack-to-shared pass to the omp dialect

--- flang/lib/Optimizer/Passes/Pipelines.cpp
+++ flang/lib/Optimizer/Passes/Pipelines.cpp
@@ -10,6 +10,7 @@
 /// common to flang and the test tools.
 
 #include "flang/Optimizer/Passes/Pipelines.h"
+#include "mlir/Dialect/OpenMP/Transforms/Passes.h"
 #include "llvm/Support/CommandLine.h"
 
 /// Force setting the no-alias attribute on fuction arguments when possible.
@@ -365,10 +366,8 @@ void createOpenMPFIRPassPipeline(mlir::PassManager &pm,
   // extracts them.
   pm.addPass(flangomp::createDeleteUnreachableTargetsPass());
   pm.addPass(flangomp::createGenericLoopConversionPass());
-  if (opts.isTargetDevice) {
-    pm.addPass(flangomp::createStackToSharedPass());
+  if (opts.isTargetDevice)
     pm.addPass(flangomp::createFunctionFilteringPass());
-  }
 }
 
 void createDebugPasses(mlir::PassManager &pm,
@@ -439,6 +438,12 @@ void createDefaultFIRCodeGenPassPipeline(mlir::PassManager &pm,
   }
 
   fir::addFIRToLLVMPass(pm, config);
+
+  // Convert applicable OpenMP stack allocations to shared memory allocations
+  // for GPU targets. This pass must run after any alloca-generating passes to
+  // ensure all are adequately accounted for.
+  if (config.EnableOpenMP && !config.EnableOpenMPSimd)
+    pm.addPass(mlir::omp::createStackToSharedPass());
 }
 
 /// Create a pass pipeline for lowering from MLIR to LLVM IR
