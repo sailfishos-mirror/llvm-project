d906fa5f370d8f495ba24f1539e922433ec22c2d
bassiounix <muhammad.m.bassiouni@gmail.com>
2026-03-05 07:48:03 +0200

    [libc][math] Qualify log with constant evaluation support

--- libc/src/__support/math/log.h
+++ libc/src/__support/math/log.h
@@ -808,7 +808,8 @@ LIBC_INLINE LIBC_CONSTEXPR double log(double x) {
   fputil::DoubleDouble r1;
 
   // Perform exact range reduction
-#ifdef LIBC_TARGET_CPU_HAS_FMA_DOUBLE
+#if defined(LIBC_TARGET_CPU_HAS_FMA_DOUBLE) &&                                 \
+    !defined(LIBC_HAS_CONSTANT_EVALUATION)
   u = fputil::multiply_add(r, m, -1.0); // exact
 #else
   uint64_t c_m = x_m & 0x3FFF'E000'0000'0000ULL;
