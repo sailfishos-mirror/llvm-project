e6cbe707576e20329a645ec8f41b14c9bf7cf980
🍌Shawn <m18824909883@163.com>
2026-04-13 22:40:42 +0800

    Fix uninitialized field warnings in union tests

--- clang/test/Analysis/cxx-uninitialized-object.cpp
+++ clang/test/Analysis/cxx-uninitialized-object.cpp
@@ -435,16 +435,14 @@ class ContainsSimpleUnionTest2 {
     float uf;
     int ui;
     char uc;
-    // TODO: we'd expect the note: {{uninitialized field 'this->u'}}
-  } u; // no-note
+  } u; // expected-note{{uninitialized field 'this->u'}}
 
 public:
-  ContainsSimpleUnionTest2() {}
+  ContainsSimpleUnionTest2() {} // expected-warning{{1 uninitialized field at the end of the constructor call}}
 };
 
 void fContainsSimpleUnionTest2() {
-  // TODO: we'd expect the warning: {{1 uninitialized field}}
-  ContainsSimpleUnionTest2(); // no-warning
+  ContainsSimpleUnionTest2();
 }
 
 class UnionPointerTest1 {
@@ -479,17 +477,15 @@ public:
   };
 
 private:
-  // TODO: we'd expect the note: {{uninitialized field 'this->uptr'}}
-  SimpleUnion *uptr; // no-note
+  SimpleUnion *uptr; // expected-note{{uninitialized pointee 'this->uptr'}}
 
 public:
-  UnionPointerTest2(SimpleUnion *uptr, char) : uptr(uptr) {}
+  UnionPointerTest2(SimpleUnion *uptr, char) : uptr(uptr) {} // expected-warning{{1 uninitialized field at the end of the constructor call}}
 };
 
 void fUnionPointerTest2() {
   UnionPointerTest2::SimpleUnion u;
-  // TODO: we'd expect the warning: {{1 uninitialized field}}
-  UnionPointerTest2(&u, int()); // no-warning
+  UnionPointerTest2(&u, int());
 }
 
 class ContainsUnionWithRecordTest1 {
