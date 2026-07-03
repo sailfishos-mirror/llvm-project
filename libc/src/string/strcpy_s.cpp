4f2222e8e1e93a82758e627376f72f91c843ca68
Victor Campos <victor.campos@arm.com>
2026-07-03 13:51:53 +0100

    Address comments

--- libc/src/string/strcpy_s.cpp
+++ libc/src/string/strcpy_s.cpp
@@ -11,41 +11,39 @@
 ///
 //===----------------------------------------------------------------------===//
 
+#include "src/string/strcpy_s.h"
 #define __STDC_WANT_LIB_EXT1__ 1
 #include "hdr/stdint_proxy.h"
 #undef __STDC_WANT_LIB_EXT1__
 #include "hdr/types/constraint_handler_t.h"
 #include "hdr/types/errno_t.h"
 #include "hdr/types/rsize_t.h"
+#include "src/__support/CPP/expected.h"
 #include "src/__support/common.h"
 #include "src/__support/constraint_handler.h"
 #include "src/__support/macros/config.h"
 #include "src/string/memory_utils/inline_memcpy.h"
-#include "src/string/strcpy_s.h"
 #include "src/string/string_utils.h"
-#include "src/string/strnlen_s.h"
 
 namespace LIBC_NAMESPACE_DECL {
 
-LLVM_LIBC_FUNCTION(errno_t, strcpy_s,
-                   (char *__restrict s1, rsize_t s1max,
-                    const char *__restrict s2)) {
-  const char *constraint_violation_msg = 0;
-  size_t count;
+LIBC_INLINE static cpp::expected<size_t, const char *>
+validate(char *__restrict s1, rsize_t s1max, const char *__restrict s2) {
+  if (s1 == nullptr)
+    return cpp::unexpected("strcpy_s: s1 cannot be null");
+  if (s2 == nullptr)
+    return cpp::unexpected("strcpy_s: s2 cannot be null");
+  if (s1max > RSIZE_MAX)
+    return cpp::unexpected("strcpy_s: s1max cannot exceed RSIZE_MAX");
+  if (s1max == 0)
+    return cpp::unexpected("strcpy_s: s1max cannot be 0");
 
-  if (s1 == 0) {
-    constraint_violation_msg = "strcpy_s: s1 cannot be null";
-  } else if (s2 == 0) {
-    constraint_violation_msg = "strcpy_s: s2 cannot be null";
-  } else if (s1max > RSIZE_MAX) {
-    constraint_violation_msg = "strcpy_s: s1max cannot exceed RSIZE_MAX";
-  } else if (s1max == 0) {
-    constraint_violation_msg = "strcpy_s: s1max cannot be 0";
-  } else if (count = strnlen_s(s2, s1max);
-             s1max == count) { // count can't be greater than s1max by
-                               // definition, so no need to check for this case
-    constraint_violation_msg = "strcpy_s: s1max is too small for s2";
-  }
+  // count can't be greater than s1max by definition, so no need to check for
+  // this case.
+  size_t count = internal::strnlen_s(s2, s1max);
+  if (s1max == count)
+    return cpp::unexpected("strcpy_s: s1max is too small for s2");
+
   // Check overlap using the full regions defined by the standard, including the
   // terminating null byte:
   //   destination: [s1, s1 + s1max)
@@ -53,22 +51,30 @@ LLVM_LIBC_FUNCTION(errno_t, strcpy_s,
   // Use s1max for the destination's length, not count + 1, because the
   // standard allows for overwriting the entire destination region, even if
   // s2's length is smaller than s1max.
-  else if (const uintptr_t s1_addr = reinterpret_cast<uintptr_t>(s1),
-           s2_addr = reinterpret_cast<uintptr_t>(s2);
-           s1_addr < s2_addr ? s2_addr - s1_addr < s1max
-                             : s1_addr - s2_addr < count + 1) {
-    constraint_violation_msg = "strcpy_s: s1 and s2 cannot overlap";
-  }
+  const uintptr_t s1_addr = reinterpret_cast<uintptr_t>(s1);
+  const uintptr_t s2_addr = reinterpret_cast<uintptr_t>(s2);
+  if (s1_addr < s2_addr ? s2_addr - s1_addr < s1max
+                        : s1_addr - s2_addr < count + 1)
+    return cpp::unexpected("strcpy_s: s1 and s2 cannot overlap");
 
-  if (constraint_violation_msg) {
-    if (s1 != 0 && s1max > 0 && s1max <= RSIZE_MAX) {
+  return count;
+}
+
+LLVM_LIBC_FUNCTION(errno_t, strcpy_s,
+                   (char *__restrict s1, rsize_t s1max,
+                    const char *__restrict s2)) {
+  const auto count_expected = validate(s1, s1max, s2);
+
+  if (!count_expected.has_value()) {
+    if (s1 != nullptr && s1max > 0 && s1max <= RSIZE_MAX) {
       s1[0] = '\0';
     }
-    libc_global_constraint_handler(constraint_violation_msg, 0, 1);
-    return 1;
+    libc_global_constraint_handler(count_expected.error(), 0,
+                                   LIBC_ERRNO_T_ERROR_VALUE);
+    return LIBC_ERRNO_T_ERROR_VALUE;
   }
 
-  inline_memcpy(s1, s2, count + 1);
+  inline_memcpy(s1, s2, count_expected.value() + 1);
   return 0;
 }
 
