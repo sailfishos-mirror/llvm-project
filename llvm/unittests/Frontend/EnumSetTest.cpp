1e4d5d8350c61a31582ddbf6ee0fbe0dea5f89ec
Krzysztof Parzyszek <Krzysztof.Parzyszek@amd.com>
2026-07-22 16:59:49 -0500

    Add preamble

--- llvm/unittests/Frontend/EnumSetTest.cpp
+++ llvm/unittests/Frontend/EnumSetTest.cpp
@@ -1,3 +1,11 @@
+//===- llvm/unittests/Frontend/EnumSetTest.cpp - EnumSet unit tests -------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #include "llvm/Frontend/OpenMP/OMP.h"
 
 #include "gmock/gmock.h"
