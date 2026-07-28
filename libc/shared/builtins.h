1984097bcfe9f508058923011734eb1f4707226a
hulxv <hulxxv@gmail.com>
2026-07-29 01:29:11 +0300

    [compiler-rt][builtins] libc-backed quad-float comparison builtins

--- libc/shared/builtins.h
+++ libc/shared/builtins.h
@@ -29,8 +29,10 @@
 #include "builtins/extendxftf2.h"
 #include "builtins/gedf2.h"
 #include "builtins/gesf2.h"
+#include "builtins/getf2.h"
 #include "builtins/ledf2.h"
 #include "builtins/lesf2.h"
+#include "builtins/letf2.h"
 #include "builtins/muldf3.h"
 #include "builtins/mulsf3.h"
 #include "builtins/multf3.h"
@@ -45,5 +47,6 @@
 #include "builtins/trunctfxf2.h"
 #include "builtins/unorddf2.h"
 #include "builtins/unordsf2.h"
+#include "builtins/unordtf2.h"
 
 #endif // LLVM_LIBC_SHARED_BUILTINS_H
