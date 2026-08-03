d218bd328cc60c9a35eebae9dbf8d7579992668f
Gheorghe-Teodor Bercea <dobercea@amd.com>
2026-08-04 01:38:17 +0300

    Set up M0 for VGPR-memory accesses in finalizeLowering instead of a separate pass

--- llvm/lib/Target/AMDGPU/AMDGPU.h
+++ llvm/lib/Target/AMDGPU/AMDGPU.h
@@ -223,9 +223,6 @@ extern char &AMDGPURegBankLegalizeID;
 void initializeAMDGPUMarkLastScratchLoadLegacyPass(PassRegistry &);
 extern char &AMDGPUMarkLastScratchLoadID;
 
-void initializeAMDGPUAssignIdxToM0LegacyPass(PassRegistry &);
-extern char &AMDGPUAssignIdxToM0ID;
-
 void initializeSILowerSGPRSpillsLegacyPass(PassRegistry &);
 extern char &SILowerSGPRSpillsLegacyID;
 
@@ -438,13 +435,6 @@ class AMDGPUMarkLastScratchLoadPass
 public:
   PreservedAnalyses run(MachineFunction &MF,
                         MachineFunctionAnalysisManager &AM);
-};
-
-class AMDGPUAssignIdxToM0Pass
-    : public RequiredPassInfoMixin<AMDGPUAssignIdxToM0Pass> {
-public:
-  PreservedAnalyses run(MachineFunction &MF,
-                        MachineFunctionAnalysisManager &MFAM);
 };
 
 class SIInsertWaitcntsPass
