9ddc5d136786274045313e58584c84d5ee16ca05
Sukumarsawant <sawantsukumar@gmail.com>
2026-08-06 16:56:36 +0530

    add explicit bit handling for dyadic_float as per FPBits

--- libc/src/__support/FPUtil/dyadic_float.h
+++ libc/src/__support/FPUtil/dyadic_float.h
@@ -261,6 +261,8 @@ template <size_t Bits> struct DyadicFloat {
       MantissaType sticky_mask = round_mask - 1;
       sticky = (mantissa & sticky_mask) != 0;
       out_mantissa = static_cast<StorageType>(mantissa >> extra_fraction_len);
+      if constexpr (get_fp_type<T>() == FPType::X86_Binary80)
+        out_mantissa |= FPBits::EXPLICIT_BIT_MASK;
     }
 
     bool lsb = (out_mantissa & 1) != 0;
