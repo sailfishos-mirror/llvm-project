3cfc63f828e1a5ac1b4d400cdc9561a4c1ea5005
Zorojuro <sawantsukumar@gmail.com>
2026-08-21 17:52:48 +0530

    Apply suggestion from @Sukumarsawant

--- libc/test/src/math/smoke/fdimf128_test.cpp
+++ libc/test/src/math/smoke/fdimf128_test.cpp
@@ -7,8 +7,8 @@
 //===----------------------------------------------------------------------===//
 
 #include "FDimTest.h"
-#include "src/__support/FPUtil/float128.h"
 
+#include "src/__support/FPUtil/float128.h"
 #include "src/math/fdimf128.h"
 
 #ifndef LIBC_TYPES_HAS_NATIVE_FLOAT128
