656af1b8d18f4ff418cf34141e2a3d65fc9c2891
Zorojuro <sawantsukumar@gmail.com>
2026-08-21 17:50:06 +0530

    Apply suggestions from code review
    Co-authored-by: Zorojuro <sawantsukumar@gmail.com>

--- libc/test/src/math/smoke/fminimum_magf128_test.cpp
+++ libc/test/src/math/smoke/fminimum_magf128_test.cpp
@@ -7,8 +7,8 @@
 //===----------------------------------------------------------------------===//
 
 #include "FMinimumMagTest.h"
-#include "src/__support/FPUtil/float128.h"
 
+#include "src/__support/FPUtil/float128.h"
 #include "src/math/fminimum_magf128.h"
 
 #ifndef LIBC_TYPES_HAS_NATIVE_FLOAT128
